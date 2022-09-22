08f18142bdef8cd78b1b5b5bc4d7d81ec83f2bdd
Gui-Gos <97973228+Gui-Gos@users.noreply.github.com>
2022-09-22 14:41:02 +0200

    ok

---
+++ FULL.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+################################################################
+#  Magic_infra
+################################################################
+#
+# Version 2.0 - 2022-09-22
+#
+# By Gui-Gos
+#
+################################################################
+
+
+######################################################################
+# set color variables fo printf or echo
+Black="\e[0;30m"
+Red="\e[0;31m"
+Green="\e[0;32m"
+Yellow="\e[0;33m"
+Blue="\e[0;34m"
+Purple="\e[0;35m"
+Cyan="\e[0;36m"
+White="\e[0;37m"
+Grey="\e[0;39m"
+ResetColor="\e[0m"
+# example of printf > printf "${Green}"$VARIABLE" or text${ResetColor}\n" <
+
+
