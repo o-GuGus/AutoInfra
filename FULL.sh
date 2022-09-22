9a2f3614ee8d58931fab2809c5954e0e27282dcf
Gui-Gos <97973228+Gui-Gos@users.noreply.github.com>
2022-09-22 16:11:53 +0200

    0

--- FULL.sh
+++ FULL.sh
@@ -11,6 +11,8 @@
 
 
 ######################################################################
+# clear screen
+clear
 # set color variables fo printf or echo
 Black="\e[0;30m"
 Red="\e[0;31m"
@@ -24,4 +26,54 @@ Grey="\e[0;39m"
 ResetColor="\e[0m"
 # example of printf > printf "${Green}"$VARIABLE" or text${ResetColor}\n" <
 
+
+
+
+function BANNER {
+printf " ██████╗ ██╗   ██╗██╗██╗     ██╗      █████╗ ██╗   ██╗███╗   ███╗███████╗
+██╔════╝ ██║   ██║██║██║     ██║     ██╔══██╗██║   ██║████╗ ████║██╔════╝
+██║  ███╗██║   ██║██║██║     ██║     ███████║██║   ██║██╔████╔██║█████╗
+██║   ██║██║   ██║██║██║     ██║     ██╔══██║██║   ██║██║╚██╔╝██║██╔══╝
+╚██████╔╝╚██████╔╝██║███████╗███████╗██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗
+╚═════╝  ╚═════╝ ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n"
+}
+
+
+
+
+
+######################################################################
+# check sudo or root perms #
+
+function RootorUser {
+	WhoIs=`pwd`
+	Name=`whoami`
+	if [ sudo -l | grep -c "may not run" -eq 1]; then
+		printf "\n${Blue}$Name ${Red}is not a sudoers account${ResetColor}\n"
+			if [ $Name = "root" ]; then
+				printf "\n${Blue}$Name ${Red}is a good account${ResetColor}\n"
+			else
+				printf "\n${Yellow}Please go to 'root' account to continue${ResetColor}\n"
+				su root && bash $0
+			fi
+	else
+		printf "\n${Green}This a sudoers account${ResetColor}\n"
+	fi
+	
+
+RootorUser
+
+
+
+
+
+
+
+################################################################
+#  SCRIPT START HERE
+################################################################
+
+
+BANNER
+RootorUser
 
